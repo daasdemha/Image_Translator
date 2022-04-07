ffca74bce7183785234ebc02f04703e6fc4ae416
saad iftikhar <daasdemha@hotmail.com>
2022-04-07 13:39:14 +0100

    Home view model and view created and azure api services used

---
+++ ImagesTranslator/ImagesTranslator/ImagesTranslator/Utility/IAzureService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImagesTranslator.Utility
+{
+    public interface IAzureService
+    {
+        Task<OcrResult> GetOCRResult(MediaFile file);
+    }
+}
---
+++ ImagesTranslator/ImagesTranslator/ImagesTranslator/Views/DetailsPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Plugin.Media.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ImagesTranslator.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetailsPage : ContentPage
+    {
+        private MediaFile photo;
+       
+        public DetailsPage(MediaFile photo, OcrResult result)
+        {
+            InitializeComponent();
+            
+            this.photo = photo;
+            Image.Source = ImageSource.FromStream(() =>
+            {
+                var a = photo.GetStream();
+                return a;
+            });
+            foreach (var region in result.Regions)
+            {
+               
+                foreach (var line in region.Lines)
+                {
+                   
+
+                    foreach (var word in line.Words)
+                    {
+                        LblResult.Text += Convert.ToString(word.Text)+" ";
+                        
+                        
+                    }
+                    LblResult.Text += "\n";
+                }
+                
+            }
+          
+        }
+        
+
+        private async void ShareNote_Clicked(object sender, EventArgs e)
+        {
+            if (LblResult != null)
+            {
+                await Share.RequestAsync(new ShareTextRequest
+                {
+
+                    Text = LblResult.Text,
+                    Title = "Notes"
+                });
+            }
+        }
+    }
+}
